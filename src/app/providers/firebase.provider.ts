d858636c68ef9f8266822458219bb56090b7d42a
tiffan-source <zohoungbogboanlyou@gmail.com>
2026-04-09 16:06:42 +0200

    feat: refactor Firebase provider to use centralized FIREBASE_CONFIG for configuration

--- src/app/providers/firebase.provider.ts
+++ src/app/providers/firebase.provider.ts
@@ -1,18 +1,11 @@
 import { Provider } from "@angular/core";
 import { FirebaseAppService } from "libs/infrastructure/src/lib/repositories/firebase/firebase-app";
 import { AppFirebaseConfig } from "libs/infrastructure/src/lib/repositories/firebase/firebase-app";
+import { FIREBASE_CONFIG } from "src/env/env";
 
 export const FIREBASE_PROVIDERS: Provider[] = [
     {provide: FirebaseAppService, useFactory:() => {
-        const config: AppFirebaseConfig = {
-            apiKey: "AIzaSyAQwDViS507UR4CNxUpNiKkEMcvGuWAwog",
-            authDomain: "btprelance.firebaseapp.com",
-            projectId: "btprelance",
-            storageBucket: "btprelance.firebasestorage.app",
-            messagingSenderId: "557005352195",
-            appId: "1:557005352195:web:89e05e05d7901c94f20d36",
-            measurementId: "G-B9PSDWZK3Y"
-        };
+        const config: AppFirebaseConfig = FIREBASE_CONFIG;
         return new FirebaseAppService(config);
     }}
 ];
