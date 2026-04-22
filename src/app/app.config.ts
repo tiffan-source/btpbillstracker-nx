276471453fbeddea71afb5adf5c4456571caf89a
tiffan-source <zohoungbogboanlyou@gmail.com>
2026-04-22 17:30:03 +0200

    feat(config): refactor provideAppConfig to use envAppConfig for Firebase and Supabase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,11 +12,12 @@ import { CHANTIER_PROVIDERS } from './providers/chantiers.provider';
 import { provideFirebase } from './providers/firebase.provider';
 import { BILL_PROVIDERS } from './providers/bills.provider';
 import { MessageService } from 'primeng/api';
-import { FIREBASE_CONFIG } from '../env/env';
+import { envAppConfig } from '../env/env';
 import { AUTH_PROVIDERS } from './providers/auth.provider';
 import { REMINDER_PROVIDERS } from './providers/reminder.provider';
+import { provideSupabase } from './providers/supabase.provider';
 
-export function provideAppConfig(config: AppFirebaseConfig = FIREBASE_CONFIG): ApplicationConfig {
+export function provideAppConfig(): ApplicationConfig {
   return {
     providers: [
       provideBrowserGlobalErrorListeners(),
@@ -29,7 +30,8 @@ export function provideAppConfig(config: AppFirebaseConfig = FIREBASE_CONFIG): A
             }  
         },
       }),
-      ...provideFirebase(config),
+      ...provideFirebase(envAppConfig.firebase),
+      ...provideSupabase(envAppConfig.supabase),
       ...CLIENT_PROVIDERS,
       ...CHANTIER_PROVIDERS,
       ...BILL_PROVIDERS,
